2f8b74e9a8ca949883cfb54f33567e1421c96807
JakobG5 <yakobgirmad@gmail.com>
2026-05-05 17:05:05 +0300

    chore: consolidate project structure for Vercel

--- The Ledger/src/components/Get-Started.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import getHeader from '@/packages/Get-Started/get-header.vue';
-import getPage from '@/packages/Get-Started/get-page.vue';
-</script>
-<template>
-<getHeader></getHeader>
-<getPage></getPage>
-</template>
--- The Ledger/src/components/Log-in.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import logHeader from '@/packages/Log-in/log-header.vue';
-import logPage from '@/packages/Log-in/log-page.vue';
-</script>
-<template>
-<logHeader/>
-<logPage/>
-</template>
